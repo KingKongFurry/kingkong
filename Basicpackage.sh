d382456b174a5202717da956816465b305785045
KingKongFurry <65806483+KingKongFurry@users.noreply.github.com>
2020-05-29 04:03:21 -0600

    Create Basicpackage.sh

---
+++ Basicpackage.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install python
+pkg install python2
+pkg install nano
+pkg install curl
+pkg install wget
+pkg install php
+pkg install perl
+pkg install ruby
+apt update
+apt upgrade
